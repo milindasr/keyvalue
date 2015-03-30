f02b64ccef406f222fdc470aed1b27f6e126c7e2
mrsreena <mrsreena@ncsu.edu>
2015-03-30 00:26:49 -0400

    replication implemented

--- src/slaves/SlaveReplicationClient.java
+++ src/slaves/SlaveReplicationClient.java
@@ -1,5 +1,46 @@
 package slaves;
 
-public class SlaveReplicationClient {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class SlaveReplicationClient {
+	public void sendPutrequest(int key,String value,String host,int port){
+	
+		String putString="PUT "+key+" "+value;
+		serverHandler(host,port,putString);
+
+	}
+	@SuppressWarnings("finally")
+	private String serverHandler(String serverName, int port,String requestString) {
+		String response="";
+		try
+		{
+		   System.out.println("CLIENT Connecting to " + serverName
+		                       + " on port " + port);
+		   Socket client = new Socket(serverName, port);
+		   System.out.println("CLIENT Just connected to "
+		                + client.getRemoteSocketAddress());
+		   OutputStream outToServer = client.getOutputStream();
+		   DataOutputStream out =
+		                 new DataOutputStream(outToServer);
+
+		   out.writeUTF(requestString);
+		   InputStream inFromServer = client.getInputStream();
+		   DataInputStream in =
+		                  new DataInputStream(inFromServer);
+		   response=in.readUTF();
+		   client.close();
+		   return response;
+		}catch(IOException e)
+		{
+		   e.printStackTrace();
+		}
+		finally{
+			return response;
+		}
+	}
 }
--- src/slaves/SlaveServer.java
+++ src/slaves/SlaveServer.java
@@ -1,6 +1,7 @@
 package slaves;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,9 +22,11 @@ public class SlaveServer implements Runnable{
     protected ServerSocket serverSocket = null;
     protected boolean      isStopped    = false;
     protected Thread       runningThread= null;
+    protected String       path="";
 
-    public SlaveServer(int port){
+    public SlaveServer(int port,String path){
         this.serverPort = port;
+        this.path=path;
     }
 
     public void run(){
@@ -61,30 +64,61 @@ public class SlaveServer implements Runnable{
         DataInputStream in =
                 new DataInputStream(input);
         String request=in.readUTF();
-        
+        DataOutputStream out =
+				new DataOutputStream(output);
+		out.writeUTF("OK");
+		
         processRequest(request);
         output.close();
         input.close();
      
     }
     public void processRequest(String request){
-    	Scanner sc=new Scanner(request.trim());
-		String type=sc.next();
-		if(type.equals("PUT")){
-			int key=sc.nextInt();
-			String value=sc.next();
-			put(key,value);
-			sc.close();
-		}
-	}
+    	if(request.startsWith("PUT")){
+    		if(request.contains("::")){
+    			String[] splits=request.split("::");
+    			String put=splits[0];
+    			Scanner sc=new Scanner(put);
+    			sc.next();
+    			int key=sc.nextInt();
+    			String value=sc.next();
+    			sc.close();
+    			put(key,value);
+    			String nextNode=splits[1];
+    			Scanner sc1=new Scanner(nextNode);
+    			String host1=sc1.next();
+    			int port1=sc1.nextInt();
+    			String nextNode1=splits[2];
+    			sc1.close();
+    			Scanner sc2=new Scanner(nextNode1);
+    			String host2=sc2.next();
+    			int port2=sc2.nextInt();
+    			sc2.close();
+    			SlaveReplicationClient repclient1=new SlaveReplicationClient();
+    			repclient1.sendPutrequest(key, value, host1, port1);
+    			SlaveReplicationClient repclient2=new SlaveReplicationClient();
+    			repclient2.sendPutrequest(key, value, host2, port2);
+    			
+    			
+    		}
+    		else{
+    			Scanner sc=new Scanner(request);
+    			sc.next();
+    			int key=sc.nextInt();
+    			String value=sc.next();
+    			sc.close();
+    			put(key,value);
+    		}
+    	}
+    }
     private void put(int key,String value){
     	try {
-			FileWriter file=new FileWriter("C:\\Users\\Milind\\Desktop\\dbms code\\inteview\\db.txt",true);
+			FileWriter file=new FileWriter(path,true);
 			PrintWriter pw=new PrintWriter(file);
 			pw.print(key+" "+value+"\n");
 			pw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
     }
